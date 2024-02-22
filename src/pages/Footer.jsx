29eaace485464f2d75a15ce19aa967023a9e5ac6
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2024-02-22 11:58:29 +0100

    animating the footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { socialMediaLinks } from "../components/Social.jsx";
 import { Social } from "../components/Social.jsx";
+import { motion } from "framer-motion";
 
 const links = [
   { to: "/", label: "A propos" },
@@ -16,14 +17,21 @@ function Footer() {
       <div className="flex justify-between items-center">
         <ul className="hidden lg:flex sm:justify-between items-center sm:mx-2 gap-4">
           {links.map((link, index) => (
-            <li
+            <motion.li
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.5,
+                easing: "ease",
+                delay: 0.25 * index,
+              }}
               key={index}
               className="px-8 py-2 border-r border-yellow-400 text-xs "
             >
               <Link to={link.to} className="text-blue-900 font-medium">
                 {link.label}
               </Link>
-            </li>
+            </motion.li>
           ))}
         </ul>
         <div className="gap-2 flex">
